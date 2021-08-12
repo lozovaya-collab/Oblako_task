ef8e9258408a74faeaddedefc8dcfe9cdf3a96d8
anya lozovaya <anyalozovaya2014@gmail.com>
2021-08-12 16:54:38 +0400

    feat: add tabs

--- head-of-trainers/src/app/app.module.ts
+++ head-of-trainers/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { BodyComponent } from './main/body/body.component';
+import { TabsComponent } from './main/body/tabs/tabs.component';
+import { FiltersComponent } from './main/body/filters/filters.component';
+import { ListComponent } from './main/body/list/list.component';
+import { MenuComponent } from './main/body/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    BodyComponent,
+    TabsComponent,
+    FiltersComponent,
+    ListComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
